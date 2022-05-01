376b196dc81ccd5072546ababa8b12087ae41774
Mor Menashe <mormen1305@gmail.com>
2022-05-01 09:14:58 +0300

    update

--- js/game.js
+++ js/game.js
@@ -2,17 +2,11 @@ import { drawBoard, initTools, initElements, resetGame } from "./minecraft.js";
 
 const gameBoard = document.getElementById("game-board");
 
-function draw() {
-  drawBoard(gameBoard);
-}
-
 function main() {
-  draw();
+  drawBoard(gameBoard);
   initTools();
   initElements();
   resetGame();
 }
-
-// window.requestAnimationFrame(main);
 
 main();
--- js/minecraft.js
+++ js/minecraft.js
@@ -1,3 +1,5 @@
+import { boards } from "./boards.js";
+
 const elementsObject = {
   0: "dirt",
   1: "grass",
@@ -13,38 +15,64 @@ const elementsAndToolsObject = {
   pickaxe: ["stone"],
 };
 
-const elementsAndInventoryObject = {
-  dirt: "dirt",
-  grass: "grass",
-  leaves: "leaves",
-  stone: "stone",
-  wood: "wood",
-};
-
-const myArray = [
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 2, 2, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 2, 2, 2, 2, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 2, 2, 2, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 4, 9, 9, 9],
-  [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 4, 9, 9, 9],
-  [9, 9, 9, 3, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 4, 9, 9, 9],
-  [9, 9, 3, 3, 3, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 4, 9, 9, 3],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
 let currentTool = null;
+let myArray = boards[Math.floor(Math.random() * boards.length)];
+
+function hover(event) {
+  event.target.classList.add("hovered-tool");
+}
+
+function undoHover(event) {
+  event.target.classList.remove("hovered-tool");
+}
+
+function chooseTool(event) {
+  if (currentTool === null) {
+    event.target.classList.add("selected-tool");
+    currentTool = event.target;
+  } else if (currentTool === event.target) {
+    event.target.classList.remove("selected-tool");
+    currentTool = null;
+  } else {
+    currentTool.classList.remove("selected-tool");
+    event.target.classList.add("selected-tool");
+    currentTool = event.target;
+  }
+}
+
+function addElementToInventory(element) {
+  let currentElementClass = element.classList[0];
+  element.classList.remove(element.classList[0]);
+  element.className = "sky";
+  element.classList.add("game-element");
+  let currentInventory = document.getElementById(currentElementClass);
+  let currentInventoryValue = parseInt(
+    currentInventory.nextElementSibling.innerHTML
+  );
+  currentInventoryValue++;
+  currentInventory.nextElementSibling.innerHTML = currentInventoryValue;
+}
+
+function drawElementFromInventory(element) {
+  element.className = currentTool.id;
+  element.classList.add("game-element");
+}
+
+function chooseElement(event) {
+  let currentElement = event.target.classList[0];
+  if (currentElement !== "sky" && currentTool) {
+    if (elementsAndToolsObject[currentTool.id].includes(currentElement)) {
+      addElementToInventory(event.target);
+    }
+  } else if (currentElement === "sky" && currentTool) {
+    let elementInventory = parseInt(currentTool.nextElementSibling.innerHTML);
+    if (elementInventory > 0) {
+      drawElementFromInventory(event.target);
+      elementInventory--;
+      currentTool.nextElementSibling.innerHTML = elementInventory;
+    }
+  }
+}
 
 export function drawBoard(gameBoard) {
   for (let i = 0; i < myArray.length; i++) {
@@ -72,28 +100,6 @@ export function initTools() {
   });
 }
 
-function hover(event) {
-  event.target.classList.add("hovered-tool");
-}
-
-function undoHover(event) {
-  event.target.classList.remove("hovered-tool");
-}
-
-function chooseTool(event) {
-  if (currentTool === null) {
-    event.target.classList.add("selected-tool");
-    currentTool = event.target;
-  } else if (currentTool === event.target) {
-    event.target.classList.remove("selected-tool");
-    currentTool = null;
-  } else {
-    currentTool.classList.remove("selected-tool");
-    event.target.classList.add("selected-tool");
-    currentTool = event.target;
-  }
-}
-
 export function initElements() {
   let elements = document.querySelectorAll(".game-element");
   elements.forEach((element) => {
@@ -101,40 +107,6 @@ export function initElements() {
     element.addEventListener("mouseover", hover);
     element.addEventListener("mouseout", undoHover);
   });
-}
-
-function chooseElement(event) {
-  let currentElement = event.target.classList[0];
-  if (currentElement !== "sky" && currentTool) {
-    if (elementsAndToolsObject[currentTool.id].includes(currentElement)) {
-      addElementToInventory(event.target);
-    }
-  } else if (currentElement === "sky" && currentTool) {
-    let elementInventory = parseInt(currentTool.nextElementSibling.innerHTML);
-    if (elementInventory > 0) {
-      drawElementFromInventory(event.target, elementInventory);
-      elementInventory--;
-      currentTool.nextElementSibling.innerHTML = elementInventory;
-    }
-  }
-}
-
-function addElementToInventory(element) {
-  let currentElementClass = element.classList[0];
-  element.classList.remove(element.classList[0]);
-  element.className = "sky";
-  element.classList.add("game-element");
-  let currentInventory = document.getElementById(currentElementClass);
-  let currentInventoryValue = parseInt(
-    currentInventory.nextElementSibling.innerHTML
-  );
-  currentInventoryValue++;
-  currentInventory.nextElementSibling.innerHTML = currentInventoryValue;
-}
-
-function drawElementFromInventory(element, elementInventory) {
-  element.className = currentTool.id;
-  element.classList.add("game-element");
 }
 
 export function resetGame() {
